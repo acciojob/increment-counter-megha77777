7dd4c561bfac01e6f69be2448c770e6378535b4e
megha77777 <141833704+megha77777@users.noreply.github.com>
2023-12-18 17:35:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let incrementBtn = document.getElementById("incrementBtn");
+let counter = document.getElementById("counter");
+let count=0;
+function incrementCount() {
+	count++;
+	alert(count);
+	counter.innerHTML = count;
+}
+incrementBtn.addEventListener("click", incrementCount);
